5dec1a38a9d45cc1aed20c544a734f8456d0b790
amandassa <amandassa504@gmail.com>
2023-03-09 16:42:15 -0300

    crawler

--- atividade5/crawler.py
+++ atividade5/crawler.py
@@ -1,15 +1,28 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import re
 
 with open('seeds.txt') as fh:
     pages = fh.readlines()
 
+
+output = open('output.html', 'w', encoding='UTF-8')
+br = "<br>\n"
 for p in pages:
     page = urllib.request.urlopen(p)
     html = str(page.read().decode('utf-8'))
     soup = BeautifulSoup(html, 'lxml')
-
-    print("Título:", soup.title.string)
-    for img in soup.find_all('img'):
-        print("src: ", img.attrs.get("src"))
-        break
+    try:
+        if (re.search('http', str(soup.img.get('src'))) == None):
+            soup.img['src'] = p+soup.img['src']
+    except:
+        pass
+    output.write("Título: ")
+    output.write(str(soup.title.get_text()))
+    output.write(br)
+    output.write("Imagem: ")
+    output.write(br)
+    output.write(str(soup.img))
+    output.write(br)
+    output.write(br)
+output.close()
