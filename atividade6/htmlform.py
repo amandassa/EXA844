ac00bbae5cbc42b622aee7e0ce6a523d88cd26f8
amandassa <amandassa504@gmail.com>
2023-03-27 23:03:39 -0300

    Script e form

---
+++ atividade6/htmlform.py
@@ -0,0 +1,25 @@
+#!c:\python37\python.exe
+# Import modules for CGI handling
+import cgi, cgitb
+cgitb.enable()
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
+# Get data from fields
+nm=form.getvalue('name')
+d=form.getvalue('data')
+text = form.getvalue('mensagem')
+print "Content-type:text/html\r\n\r\n"
+print("<html>")
+print("<head>")
+print("<title>CGI Script</title>")
+print("</head>")
+print("<body>")
+print ("<h 2>Mensagem recebida</h 2>")
+print("Name: {}".format(nm))
+print ("</br>")
+print("Data: {}".format(d))
+print ("</br>")
+print("Text : {}".format(text))
+print ("</br>")
+print("</body>")
+print ("</html>")
